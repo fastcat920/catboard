0eab413904b6a17e5d1e15a00a47cf1de42b1426
痞子胡 <pizihu@pizihudeMacBook-Pro.local>
2026-08-07 22:17:23 +0800

    fix security admin asset cache

--- resources/views/security-admin.blade.php
+++ resources/views/security-admin.blade.php
@@ -5,10 +5,10 @@
     <meta name="viewport" content="width=device-width,initial-scale=1">
     <title>节点安全 - {{$title}}</title>
     <script>window.nodeSecurity={securePath:@json($secure_path),adminUrl:@json('/'.$secure_path)};</script>
-    <link rel="stylesheet" href="/assets/security-admin/app.css?v={{$version}}">
+    <link rel="stylesheet" href="/assets/security-admin/app.css?v={{$version}}-{{filemtime(public_path('assets/security-admin/app.css'))}}">
 </head>
 <body>
 <div id="app"><div class="boot">正在加载节点安全中心…</div></div>
-<script src="/assets/security-admin/app.js?v={{$version}}"></script>
+<script src="/assets/security-admin/app.js?v={{$version}}-{{filemtime(public_path('assets/security-admin/app.js'))}}"></script>
 </body>
 </html>
